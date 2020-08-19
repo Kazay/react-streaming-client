c76cbc422e2d5d1a4e1c4265811dc57d76d8f518
Kevin Czaja <czajakevin@gmail.com>
2020-08-19 20:39:50 +0200

    refactor to add custom browser history

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
+import history from "../history";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -10,14 +11,14 @@ import Header from "./Header";
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/show" component={StreamShow} />
         <Route path="/streams/new" component={StreamCreate} />
         <Route path="/streams/edit" component={StreamEdit} />
         <Route path="/streams/delete" component={StreamDelete} />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };

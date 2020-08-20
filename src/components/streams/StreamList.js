b0d4df27a3cde4f614980bd75c386458659eb4e7
Kevin Czaja <czajakevin@gmail.com>
2020-08-20 23:37:30 +0200

    setting up streaming on StreamShow comp using local rtmp server and obs

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStreams } from "../../actions";
 import { Link } from "react-router-dom";
+import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
   componentDidMount() {
@@ -32,7 +32,7 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>

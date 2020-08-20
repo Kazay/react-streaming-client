2f6aa6f8e82d616934e5b9afa5a8633fca42ba2c
Kevin Czaja <czajakevin@gmail.com>
2020-08-21 00:13:45 +0200

    API server url moved inside config file

--- src/api/streams.js
+++ src/api/streams.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+import { API_SERVER_URL } from "../config";
 
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: API_SERVER_URL,
 });
--- src/configSample.js
+++ src/configSample.js
@@ -1,3 +1,4 @@
-// Google ID client Oauth 2.0
+// Google Oauth 2.0 client id
 export const CLIENT_ID = "";
 export const STREAMING_SERVER_URL = "http://localhost:8000/live/";
+export const API_SERVER_URL = "http://localhost:3001";

5991ca4f665e7bab2a0d119eb23803b77b55de2c
Kevin Czaja <czajakevin@gmail.com>
2020-08-20 23:37:30 +0200

    setting up streaming on StreamShow comp using local rtmp server and obs

--- src/config.js
+++
@@ -1,2 +0,0 @@
-export const CLIENT_ID =
-  "861985274473-e9ggaeb7qve30qkp2ngajck65pa2m8ev.apps.googleusercontent.com";
---
+++ src/configSample.js
@@ -0,0 +1,3 @@
+// Google ID client Oauth 2.0
+export const CLIENT_ID = "";
+export const STREAMING_SERVER_URL = "http://localhost:8000/live/";
